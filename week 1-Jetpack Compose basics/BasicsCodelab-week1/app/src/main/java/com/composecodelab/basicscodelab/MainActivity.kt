85a8434c8a4506704c06c8e84e06d0a116c1bf61
oreocube <oreocube13@gmail.com>
2021-11-05 20:16:48 +0900

    BasicsCodelab - State in Compose

--- week 1-Jetpack Compose basics/BasicsCodelab-week1/app/src/main/java/com/composecodelab/basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab-week1/app/src/main/java/com/composecodelab/basicscodelab/MainActivity.kt
@@ -3,11 +3,19 @@ package com.composecodelab.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.composecodelab.basicscodelab.ui.theme.BasicsCodelabweek1Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +25,7 @@ class MainActivity : ComponentActivity() {
             BasicsCodelabweek1Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -26,13 +34,46 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    // remember 를 사용하면 recomposition 으로부터 보호할 수 있어 상태가 리셋되지 않는다.
+    val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabweek1Theme {
-        Greeting("Android")
+        MyApp()
+    }
+}
+
+@Composable
+fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
