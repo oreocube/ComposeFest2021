bf4a94d1d5ddf417efe83b1ced87cf2df95429ab
oreocube <oreocube13@gmail.com>
2021-11-05 20:38:38 +0900

    BasicsCodelab - Animating your list

--- week 1-Jetpack Compose basics/BasicsCodelab-week1/app/src/main/java/com/composecodelab/basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab-week1/app/src/main/java/com/composecodelab/basicscodelab/MainActivity.kt
@@ -3,14 +3,20 @@ package com.composecodelab.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,10 +39,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     // remember 를 사용하면 recomposition 으로부터 보호할 수 있어 상태가 리셋되지 않는다.
-    val expanded = remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
 
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    //val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState( // 확장 애니메이션 효과주기
+        if (expanded) 48.dp else 0.dp,
 
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -45,15 +58,16 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    // 패딩이 절대 음수가 되지 않도록 한다. (그렇지 않으면 스프링 애니매이션 사용시 crash 발생)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
             OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
+                onClick = { expanded = !expanded }
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
@@ -69,20 +83,23 @@ fun DefaultPreview() {
 
 @Composable
 fun MyApp() {
+    // remember는 컴포저블이 composition에 의해 유지되는 동안에만 작동
+    //   var shouldShowOnboarding by remember { mutableStateOf(true) }
 
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    // rememberSaveable을 사용하면 회전과 같은 composition 변경 및 프로세스 종료에도 상태가 저장됨
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
-        OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     } else {
         Greetings()
     }
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+private fun Greetings(names: List<String> = List(1000) { "$it" }) { // 리스트 아이템 1000개 추가하기
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
