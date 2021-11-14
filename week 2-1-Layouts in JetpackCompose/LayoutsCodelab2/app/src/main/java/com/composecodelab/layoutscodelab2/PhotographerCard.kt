dfabf6cdba3c41acb1a31f69f2bc05c56a25dd3c
oreocube <oreocube13@gmail.com>
2021-11-15 00:23:19 +0900

    LayoutsCodelab - Modifiers

--- week 2-1-Layouts in JetpackCompose/LayoutsCodelab2/app/src/main/java/com/composecodelab/layoutscodelab2/PhotographerCard.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsCodelab2/app/src/main/java/com/composecodelab/layoutscodelab2/PhotographerCard.kt
@@ -6,11 +6,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,10 +22,10 @@ import com.composecodelab.layoutscodelab2.ui.theme.LayoutsCodelab2Theme
 fun PhotographerCard(modifier: Modifier = Modifier) {
     Row(
         modifier
-            // 순서가 중요하다.
-            //clickable과 padding의 순서를 반대로 하면 클릭 영역 밖에 패딩이 주어진다.
+            .padding(8.dp) // 외부 패딩
+            .clip(RoundedCornerShape(4.dp)) // 모서리 둥글게
             .clickable(onClick = { /* Ignoring onClick */ })
-            .padding(16.dp)
+            .padding(16.dp) // 내부 패딩
     ) {
         Surface( // placeholder
             modifier = Modifier.size(50.dp),
