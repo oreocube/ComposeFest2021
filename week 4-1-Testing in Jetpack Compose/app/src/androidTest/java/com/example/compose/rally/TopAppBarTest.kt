b70964dcbe1e2009a2271265ff27a79a075a18b7
oreocube <oreocube13@gmail.com>
2021-11-29 01:42:14 +0900

    Testing Codelab - Merged and unmerged Semantics trees

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,14 +1,11 @@
 package com.example.compose.rally
 
-import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onRoot
-import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.ui.components.RallyTopAppBar
-import com.example.compose.rally.ui.theme.RallyTheme
 import org.junit.Rule
 import org.junit.Test
+import java.util.*
 
 class TopAppBarTest {
 
@@ -49,21 +46,26 @@ class TopAppBarTest {
     fun rallyTopAppBarTest_currentLabelExists() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
-            RallyTheme {
-                RallyTopAppBar(
-                    allScreens = allScreens,
-                    onTabSelected = {},
-                    currentScreen = RallyScreen.Accounts
-                )
-            }
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
         }
 
-        // logcat에서 semantics tree 보기
-        composeTestRule.onRoot().printToLog("currentLabelExists")
+        // 탭 안의 Text가 표시되는지 여부를 확인하기 위해,
+        // 병합되지 않은 시맨틱 트리를 쿼리하여 useUnmergedTree = true를
+        // onRoot라는 finder에 전달할 수 있다.
+        // composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
 
-        // 대문자로 표시되는지 확인하기
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase(Locale.getDefault())) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 
